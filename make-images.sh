8c6b3a1e6c06deec21030d91c4735ff989836653
soulteary <soulteary@gmail.com>
2022-09-30 11:40:07 +0800

    chore: add build script

---
+++ make-images.sh
@@ -0,0 +1,3 @@
+docker build -t soulteary/bert-base-uncased-emotion:2022.09.30 -f docker/bert-base-uncased-emotion/Dockerfile .
+
+docker build -t soulteary/novel-zh-en:2022.09.30 -f docker/novel-zh-en/Dockerfile .
